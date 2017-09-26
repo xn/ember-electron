fa211383d5046dda2de681dc7dedd3c15beb7ffb
Ben Demboski <ben@turbopatent.com>
2017-09-26 10:47:16 -0700

    Fix packaging/making with npm
    The code was running 'npm prune', but that doesn't do anything if no dependencies are already installed. What we really want is 'npm install --production' and I also added '--no-bin-links' to match the yarn behavior. Also, added end-to-end unit tests running with npm in addition to the ones running with yarn.
    
    Fixes #308, #309

--- node-tests/acceptance/end-to-end-test.js
+++ node-tests/acceptance/end-to-end-test.js
@@ -7,6 +7,7 @@ const {
   readFileSync,
   readJsonSync,
   removeSync,
+  rename,
   writeFileSync,
   writeJsonSync,
 } = require('fs-extra');
@@ -33,37 +34,6 @@ describe('end-to-end', function() {
     }));
   }
 
-  before(function() {
-    this.timeout(10 * 60 * 1000);
-
-    let { name: tmpDir } = tmp.dirSync();
-
-    return run('yarn', ['pack', '--filename', path.join(tmpDir, 'ember-electron.tgz')]).then(() => {
-      process.chdir(tmpDir);
-
-      // yarn won't install from a gzipped tarball, and try as I might I can't
-      // get node-tar or tar.gz to untar the tarballs created by yarn or npm
-      return run('tar', ['-xzf', 'ember-electron.tgz']);
-    }).then(() => {
-      // Prevent yarn caching from screwing us
-      let packageJson = readJsonSync(path.join('package', 'package.json'));
-      packageJson.version = `packageJson.version-${new Date().getTime()}`;
-      writeJsonSync(path.join('package', 'package.json'), packageJson);
-
-      return ember('new', 'ee-test-app', '--yarn');
-    }).then(() => {
-      process.chdir('ee-test-app');
-
-      return ember('install', `ember-electron@file:${tmpDir}/package`);
-    }).then(() => {
-      // yarn has some kind of bug that causes it to hoist the wrong version of
-      // npmlog during one of the operations performed by the ember-electron
-      // blueprint, which causes things to explode. Running yarn upgrade
-      // corrects the situation.
-      return run('yarn', ['upgrade']);
-    });
-  });
-
   after(() => {
     process.chdir(rootDir);
   });
@@ -72,55 +42,131 @@ describe('end-to-end', function() {
     removeSync('electron-out');
   });
 
-  it('tests', () => {
-    return expect(ember('electron:test')).to.eventually.be.fulfilled;
-  });
+  function describe2() {}
 
-  it('builds', () => {
-    return ember('electron:build').then(() => {
-      expect(existsSync(path.join('electron-out', 'ember'))).to.be.ok;
-    });
-  });
+  describe2('with yarn', function() {
+    before(function() {
+      this.timeout(10 * 60 * 1000);
 
-  it('assembles', () => {
-    return ember('electron:assemble').then(() => {
-      expect(existsSync(path.join('electron-out', 'project'))).to.be.ok;
-    });
-  });
+      let { name: tmpDir } = tmp.dirSync();
 
-  it('packages', () => {
-    return ember('electron:package').then(() => {
-      expect(existsSync(path.join('electron-out', `ee-test-app-${process.platform}-${process.arch}`))).to.be.ok;
-    });
-  });
+      return run('yarn', ['pack', '--filename', path.join(tmpDir, 'ember-electron.tgz')]).then(() => {
+        process.chdir(tmpDir);
 
-  it('makes', () => {
-    // Only build zip target so we don't fail from missing platform dependencies
-    // (e.g. rpmbuild)
-    return ember('electron:make', '--targets', 'zip').then(() => {
-      expect(existsSync(path.join('electron-out', 'make'))).to.be.ok;
-    });
-  });
+        // yarn won't install from a gzipped tarball, and try as I might I can't
+        // get node-tar or tar.gz to untar the tarballs created by yarn or npm
+        return run('tar', ['-xzf', 'ember-electron.tgz']);
+      }).then(() => {
+        // Prevent yarn caching from screwing us
+        let packageJson = readJsonSync(path.join('package', 'package.json'));
+        packageJson.version = `packageJson.version-${new Date().getTime()}`;
+        writeJsonSync(path.join('package', 'package.json'), packageJson);
 
-  it('extra checks pass', () => {
-    let fixturePath = path.resolve(__dirname, '..', 'fixtures', 'ember-test');
+        return ember('new', 'ee-test-app', '--yarn');
+      }).then(() => {
+        process.chdir('ee-test-app');
 
-    // Append our extra test content to the end of test-main.js
-    let testMainPath = path.join('ember-electron', 'test-main.js');
-    let extraContentPath = path.join(fixturePath, 'test-main-extra.js');
-    let content = [
-      readFileSync(testMainPath),
-      readFileSync(extraContentPath),
-    ].join('\n');
-    writeFileSync(path.join('ember-electron', 'test-main.js'), content);
-
-    // Copy the lib and resources directories over
-    ['lib', 'resources'].forEach((dir) => {
-      copySync(path.join(fixturePath, dir), path.join('ember-electron', dir));
+        return ember('install', `ember-electron@file:${tmpDir}/package`);
+      }).then(() => {
+        // yarn has some kind of bug that causes it to hoist the wrong version of
+        // npmlog during one of the operations performed by the ember-electron
+        // blueprint, which causes things to explode. Running yarn upgrade
+        // corrects the situation.
+        return run('yarn', ['upgrade']);
+      });
     });
 
-    return expect(ember('electron:test')).to.eventually.be.fulfilled;
+    runTests();
   });
+
+  describe('with npm', function() {
+    let oldPath;
+
+    before(function() {
+      this.timeout(10 * 60 * 1000);
+
+      // We need to make yarn-or-npm choose npm even if yarn is installed, so
+      // we'll create a script called 'yarn' that fails to run and set it up
+      // early in the PATH
+      let { name: fakeBinDir } = tmp.dirSync();
+      let fakeYarnPath = path.join(fakeBinDir, 'yarn');
+      writeFileSync(fakeYarnPath, '#!/bin/sh\nexit 1', { mode: 0o777 });
+      oldPath = process.env.PATH;
+      process.env.PATH = `${fakeBinDir}:${process.env.PATH}`;
+
+      let { name: tmpDir } = tmp.dirSync();
+
+      return run('npm', ['pack'], { stdio: 'pipe' }).then(({ stdout: filename }) => {
+        return rename(filename, path.join(tmpDir, 'ember-electron.tgz'));
+      }).then(() => {
+        process.chdir(tmpDir);
+
+        return ember('new', 'ee-test-app');
+      }).then(() => {
+        process.chdir('ee-test-app');
+
+        return ember('install', `ember-electron@file:${tmpDir}/ember-electron.tgz`);
+      });
+    });
+
+    after(() => {
+      process.env.PATH = oldPath;
+    });
+
+    runTests();
+  });
+
+  function runTests() {
+    it('tests', () => {
+      return expect(ember('electron:test')).to.eventually.be.fulfilled;
+    });
+
+    it('builds', () => {
+      return ember('electron:build').then(() => {
+        expect(existsSync(path.join('electron-out', 'ember'))).to.be.ok;
+      });
+    });
+
+    it('assembles', () => {
+      return ember('electron:assemble').then(() => {
+        expect(existsSync(path.join('electron-out', 'project'))).to.be.ok;
+      });
+    });
+
+    it('packages', () => {
+      return ember('electron:package').then(() => {
+        expect(existsSync(path.join('electron-out', `ee-test-app-${process.platform}-${process.arch}`))).to.be.ok;
+      });
+    });
+
+    it('makes', () => {
+      // Only build zip target so we don't fail from missing platform dependencies
+      // (e.g. rpmbuild)
+      return ember('electron:make', '--targets', 'zip').then(() => {
+        expect(existsSync(path.join('electron-out', 'make'))).to.be.ok;
+      });
+    });
+
+    it('extra checks pass', () => {
+      let fixturePath = path.resolve(__dirname, '..', 'fixtures', 'ember-test');
+
+      // Append our extra test content to the end of test-main.js
+      let testMainPath = path.join('ember-electron', 'test-main.js');
+      let extraContentPath = path.join(fixturePath, 'test-main-extra.js');
+      let content = [
+        readFileSync(testMainPath),
+        readFileSync(extraContentPath),
+      ].join('\n');
+      writeFileSync(path.join('ember-electron', 'test-main.js'), content);
+
+      // Copy the lib and resources directories over
+      ['lib', 'resources'].forEach((dir) => {
+        copySync(path.join(fixturePath, dir), path.join('ember-electron', dir));
+      });
+
+      return expect(ember('electron:test')).to.eventually.be.fulfilled;
+    });
+  }
 });
 
 function listenForPrompts(child) {
